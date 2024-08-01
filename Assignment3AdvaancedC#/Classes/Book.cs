21c45a15c190892c373c869f3b90d29c67b8aaf3
a7medsaber10 <ahmedsa2522003@gmail.com>
2024-08-02 02:03:02 +0300

    Part1 Delegate

---
+++ Assignment3AdvaancedC#/Classes/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3AdvaancedC_.Classes
+{
+    internal class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublictionDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublictionDate, decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublictionDate = _PublictionDate;
+            Price = _Price;            
+        }
+
+        public override string ToString()
+        {
+            return $"ISBN: {ISBN}, Title: {Title}, Price: {Price}";
+        }
+
+    }
+}
+
+
--- Assignment3AdvaancedC#/Program.cs
+++ Assignment3AdvaancedC#/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Assignment3AdvaancedC_
+﻿using Assignment3AdvaancedC_.Classes;
+
+
+
+namespace Assignment3AdvaancedC_
 {
+    internal delegate string BookDelegate(Book book);
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Book> books = new List<Book>
+            {
+                new Book("1234567", "Book1", new string[] {"Author1"}, DateTime.Now, 22.5m),
+                new Book("9874561", "Book2", new string[] {"Author2"}, DateTime.Now, 30.7m)
+            };
+
+            // using user-defined delegate
+            BookDelegate TitleDelegate = BookFunctions.GetTitle;
+            LibraryEngine.ProcessBooks(books, TitleDelegate);
+
+            // using built-in delegate
+            Func<Book, string> priceFunc = BookFunctions.GetPrice;
+            LibraryEngine.ProcessBooksBuilltIn(books, priceFunc);
+
+            // Anonymos method
+            LibraryEngine.ProcessBooks(books, delegate (Book b) { return b.ISBN; });
+
+            // lambda expression
+            LibraryEngine.ProcessBooks(books, b => b.PublictionDate.ToString("d"));
         }
     }
 }
